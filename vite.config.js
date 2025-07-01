fdc4b659d77a03d9cecbee4bea8434a815084c12
JoseuthS <Josueth.Salverredy@outlook.com>
2025-07-01 01:13:47 -0400

    Always runs on port 3000

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    port: 3000, // Always use port 3000
+  },
   plugins: [
     remix({
       future: {
